90dfc414376ceb32b4df5c9eb7ff944b8c99aa0a
sianotalone <sianotalone@gmail.com>
2024-01-04 22:34:49 +0100

    add episodes handlers and some fixes

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -46,9 +47,40 @@ type Episode struct {
 	Name   string `json:"name"`
 	Number int    `json:"number"`
 }
+
+type Delete_episode struct {
+	ID int `json:"id"`
+}
 type Episode_response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+type Add_news struct {
+	Episode_ID int `json:"episode_id"`
+}
+type Episode_db struct {
+	ID       int       `json:"id"`
+	Name     string    `json:"name"`
+	Number   int       `json:"number"`
+	Date     time.Time `json:"date"`
+	Released bool      `json:"released"`
+}
+
+type Episode_notation struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Url      string `json:"url"`
+	Short    string `json:"short"`
+	Origin   string `json:"origin"`
+	Preview  string `json:"preview"`
+	Notation string `json:"notation"`
+}
+type Notation struct {
+	ID       int    `json:"id"`
+	Notation string `json:"notation"`
+}
+type Notation_del struct {
+	ID int `json:"id"`
 }
 
 func init() {
@@ -82,12 +114,194 @@ func main() {
 	r.HandleFunc("/news/films", films_news_page)
 
 	r.HandleFunc("/episode/new/", new_episode).Methods(http.MethodPost)
+	r.HandleFunc("/episode/delete/", delete_episode).Methods(http.MethodPost)
+	r.HandleFunc("/episode/add_news/", add_news_from_favorit).Methods(http.MethodPost)
+	r.HandleFunc("/episode/get_all", get_all_episodes)
+	r.HandleFunc("/episode/get/{id}", get_episode_by_id)
+	r.HandleFunc("/episode/notation/update/", update_notation).Methods(http.MethodPost)
+	r.HandleFunc("/episode/notation/delete/", delete_notation).Methods(http.MethodPost)
 	corsHandler := handlers.CORS(headers, methods, origins)(r)
 
 	http.ListenAndServe(":80", corsHandler)
 }
 
-<<<<<<< HEAD
+func delete_notation(w http.ResponseWriter, r *http.Request) {
+	var id Notation_del
+	json.NewDecoder(r.Body).Decode(&id)
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		logger.Warn(err)
+	}
+	req := "update allnews.notation set deleted=true where id=$1;"
+	_, err = db.Exec(req, id.ID)
+	if err != nil {
+		logger.Warn(err)
+	}
+	db.Close()
+	var resp Episode_response
+	resp.Status = "200, OK"
+	resp.Message = "Успішно видалено нотатку"
+	json.NewEncoder(w).Encode(resp)
+	logger.Info(fmt.Sprintf("Delete notation with ID %d", id.ID))
+}
+func update_notation(w http.ResponseWriter, r *http.Request) {
+	var notation Notation
+	json.NewDecoder(r.Body).Decode(&notation)
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		logger.Warn(err)
+	}
+	req := "update allnews.notation set notation=$1 where id=$2;"
+	_, err = db.Exec(req, notation.Notation, notation.ID)
+	if err != nil {
+		logger.Warn(err)
+	}
+	db.Close()
+	var resp Episode_response
+	resp.Status = "200, OK"
+	resp.Message = "Успішно оновлено нотатку"
+	json.NewEncoder(w).Encode(resp)
+	logger.Info(fmt.Sprintf("Update notation with ID %d", notation.ID))
+
+}
+func get_episode_by_id(w http.ResponseWriter, r *http.Request) {
+	var notations []Episode_notation
+	vars := mux.Vars(r)
+	str_id := vars["id"]
+	id, err := strconv.Atoi(str_id)
+
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		logger.Warn(err)
+	}
+	req := `select dc.id, news.title, news.url, news.short, news.origin, news.preview, dc.notation
+	from allnews.notation dc
+	left join allnews.games_news news on dc.news_id = news.id
+	where dc.deleted=false and dc.episode_id=`
+	req += str_id
+	rows, err := db.Query(req)
+	for rows.Next() {
+		e := Episode_notation{}
+		err = rows.Scan(&e.ID, &e.Title, &e.Url, &e.Short, &e.Origin, &e.Preview, &e.Notation)
+		if err != nil {
+			logger.Warn(err)
+		}
+		notations = append(notations, e)
+	}
+	db.Close()
+	json.NewEncoder(w).Encode(notations)
+	logger.Info("Get episode with ID ", id)
+
+}
+func get_all_episodes(w http.ResponseWriter, r *http.Request) {
+	var episodes []Episode_db
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		logger.Warn(err)
+	}
+	req := "select id,name,number,date,released from allnews.episode"
+	rows, err := db.Query(req)
+	for rows.Next() {
+		e := Episode_db{}
+		err = rows.Scan(&e.ID, &e.Name, &e.Number, &e.Date, &e.Released)
+		if err != nil {
+			log.Fatal(err)
+		}
+		episodes = append(episodes, e)
+	}
+	db.Close()
+	json.NewEncoder(w).Encode(episodes)
+	logger.Info("Get all episodes")
+
+}
+func add_news_from_favorit(w http.ResponseWriter, r *http.Request) {
+	var news Add_news
+	var news_count int
+	news_count = 0
+	json.NewDecoder(r.Body).Decode(&news)
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	req :=
+		`select * from allnews.games_news
+	where favorit = true
+	`
+	rows, err := db.Query(req)
+	var gaming_news []Games_News
+	for rows.Next() {
+		f := Games_News{}
+		err = rows.Scan(&f.Id, &f.Title, &f.Short, &f.Origin, &f.Url, &f.Preview, &f.Time, &f.Favorit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		gaming_news = append(gaming_news, f)
+	}
+
+	for _, value := range gaming_news {
+		req = "select count(news_id) from allnews.notation where news_id=$1 and episode_id=$2;"
+		var count int
+		rows, err := db.Query(req, value.Id, news.Episode_ID)
+		if err != nil {
+			logger.Error(err)
+		}
+		for rows.Next() {
+			err = rows.Scan(&count)
+			if err != nil {
+				logger.Error(err)
+			}
+
+		}
+		if count == 0 {
+			news_count += 1
+			req = "INSERT INTO allnews.notation (episode_id, news_id, notation) VALUES ($1, $2, '');"
+			_, err = db.Exec(req, news.Episode_ID, value.Id)
+			if err != nil {
+				logger.Error(err)
+			}
+		}
+
+	}
+	db.Close()
+	var resp Episode_response
+	resp.Status = "200, OK"
+	var mess string
+	if news_count == 0 {
+		mess = fmt.Sprintf("Новин не додано до випуску %d", news.Episode_ID)
+	} else {
+		mess = fmt.Sprintf("Новини додано у випуск %d у кількості %d", news.Episode_ID, news_count)
+	}
+
+	resp.Message = mess
+	json.NewEncoder(w).Encode(resp)
+	logger.Info(fmt.Sprintf("Add news to episode with ID %d with count %d", news.Episode_ID, news_count))
+
+}
+func delete_episode(w http.ResponseWriter, r *http.Request) {
+	var episode Delete_episode
+	json.NewDecoder(r.Body).Decode(&episode)
+	db, err := sql.Open("postgres", "postgres://postgres:12345678@localhost/news?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := "DELETE FROM allnews.episode WHERE id = $1;"
+	_, err = db.Exec(req, episode.ID)
+	if err != nil {
+		panic(err)
+	}
+	db.Close()
+	var resp Episode_response
+	resp.Status = "200, OK"
+	mess := fmt.Sprintf("Успішно видалено епізод із ID %d", episode.ID)
+	resp.Message = mess
+	json.NewEncoder(w).Encode(resp)
+	logger.Info(fmt.Sprintf("Delete episode with ID %d", episode.ID))
+}
+
 func new_episode(w http.ResponseWriter, r *http.Request) {
 
 	var episode Episode
@@ -113,8 +327,6 @@ func new_episode(w http.ResponseWriter, r *http.Request) {
 
 }
 
-=======
->>>>>>> a06771b6f349b9e23a8d5f02e19b21e7ac77ea7c
 func get_gaming_news_byorigin_page_number(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	str_id := vars["id"]
